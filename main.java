78b580097db4a05025fb8e329eae69a25b4602e0
joonyou <yaheahaha@gmail.com>
2023-12-02 04:25:56 +0200

    Co-authored-by: Kareem Hatem <ERRORsOverflow@users.noreply.github.com> Co-authored-by: Kareem Essam <KareemEssam7@users.noreply.github.com>
    switched from hashmap to chad joined list

--- main.java
+++ main.java
@@ -1,3 +1,4 @@
+import CustomStructures.*;
 class main {
     public static void main(String[] args) {
 
@@ -6,7 +7,7 @@ class main {
         user tmp3 = new user("chinese@gmail.com", "chinese ", "chinese123", 'M', "3/3/3");
         user tmp4 = new user("khater@gmail.com", "khater", "khater123", 'M', "4/4/4");
         user tmp5 = new user("FraudHatem@gmail.com", "kareem", "kareem123", 'M', "5/5/5");
-        user tmp6 = new user("joon@gmail.com", "yehia", "yehia123", 'M', "6/6/6");
+        user tmp6 = new user("joon@gmail.com", "yehia", "yehia123", 'D', "6/6/6");
         user tmp7 = new user("kareemessam@gmail.com", "kareem", "kareem123", 'M', "7/7/7");
         tmp2.addFriend(tmp3);
         tmp2.addFriend(tmp4);
@@ -15,11 +16,11 @@ class main {
         System.out.println(tmp3.friends.size());
         System.out.println(tmp2.friends.size());
         tmp2.CreatePost("test");
-        for (Integer i : tmp2.posts) {
-            System.out.println(FBsystem.posts.get(i).content);
-            System.out.println(FBsystem.posts.get(i).ReactorsID.size());
-            tmp4.like(FBsystem.posts.get(i));
-            System.out.println(FBsystem.posts.get(i).ReactorsID.size());
+        for (Node<Post> it = FBsystem.posts.iteratorToStart(); it != null; it = FBsystem.posts.nextValue()) {
+            System.out.println(it.value().content);
+            System.out.println(it.value().ReactorsID.size());
+            tmp4.like(it.value());
+            System.out.println(it.value().ReactorsID.size());
         }
     }
 }
