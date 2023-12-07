46c83f028fd2267aa1c4eb0f9f424182b9445560
Kareem Essam <kareemessam2101@gmail.com>
2023-12-07 18:32:56 +0200

    Co-authored-by: iiYusuf <iiYusuf@users.noreply.github.com> Co-authored-by: CodingPanda166 <CodingPanda166@users.noreply.github.com>

--- main.java
+++ main.java
@@ -4,16 +4,21 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Vector;
 import java.util.TreeSet;
+
 class main {
     public static void main(String[] args) {
 
         // user tmp = new user("amany@gmail.com", "amany", "amany123", 'F', "1/1/1");
         // user tmp2 = new user("ayman@gmail.com", "ayman", "ayman123", 'M', "2/2/2");
-        // user tmp3 = new user("chinese@gmail.com", "chinese ", "chinese123", 'M', "3/3/3");
-        // user tmp4 = new user("khater@gmail.com", "khater", "khater123", 'M', "4/4/4");
-        // user tmp5 = new user("FraudHatem@gmail.com", "kareem", "kareem123", 'M', "5/5/5");
+        // user tmp3 = new user("chinese@gmail.com", "chinese ", "chinese123", 'M',
+        // "3/3/3");
+        // user tmp4 = new user("khater@gmail.com", "khater", "khater123", 'M',
+        // "4/4/4");
+        // user tmp5 = new user("FraudHatem@gmail.com", "kareem", "kareem123", 'M',
+        // "5/5/5");
         // user tmp6 = new user("joon@gmail.com", "yehia", "yehia123", 'D', "6/6/6");
-        // user tmp7 = new user("kareemessam@gmail.com", "kareem", "kareem123", 'M', "7/7/7");
+        // user tmp7 = new user("kareemessam@gmail.com", "kareem", "kareem123", 'M',
+        // "7/7/7");
         // tmp2.addFriend(tmp3);
         // tmp2.addFriend(tmp4);
         // System.out.println(tmp3.friends.size());
@@ -21,11 +26,12 @@ class main {
         // System.out.println(tmp3.friends.size());
         // System.out.println(tmp2.friends.size());
         // tmp2.CreatePost("test");
-        // for (Node<Post> it = FBsystem.posts.iteratorToStart(); it != null; it = FBsystem.posts.nextValue()) {
-        //     System.out.println(it.value().content);
-        //     System.out.println(it.value().ReactorsID.size());
-        //     tmp4.like(it.value());
-        //     System.out.println(it.value().ReactorsID.size());
+        // for (Node<Post> it = FBsystem.posts.iteratorToStart(); it != null; it =
+        // FBsystem.posts.nextValue()) {
+        // System.out.println(it.value().content);
+        // System.out.println(it.value().ReactorsID.size());
+        // tmp4.like(it.value());
+        // System.out.println(it.value().ReactorsID.size());
         // }
         // Vector<Integer> v = new Vector<>();
         // v.add(tmp.id);
@@ -42,7 +48,7 @@ class main {
         // System.out.println(FBsystem.conversations.get(1L).participantsID.size());
         // tmp3.LeaveConversation(1L);
         // System.out.println(FBsystem.conversations.size());
-        //testcommit123
+        // testcommit123
         user amany = new user("amany@gmail.com", "amany", "amany123", 'F', "1/1/1");
         user ayman = new user("ayman@gmail.com", "ayman", "ayman123", 'M', "2/2/2");
         user jiangly = new user("chinese@gmail.com", "chinese ", "chinese123", 'M', "3/3/3");
@@ -52,7 +58,45 @@ class main {
         user kareem = new user("kareemessam@gmail.com", "kareem", "kareem123", 'M', "7/7/7");
         user mahrous = new user("mahrousElNayem", "Mahrous", "Mahrous123", 'L', "8/8/8");
 
+        // jiangly.addFriend(ayman);
+        // jiangly.addFriend(khater);
+        // ayman.addFriend(amany);
+        // khater.addFriend(amany);
+        // amany.addFriend(wisdom);
+        // khater.addFriend(kareem);
+        // kareem.addFriend(wisdom);
+        // joon.addFriend(mahrous);
 
+        // jiangly.block(amany.getId());
+        // jiangly.unblock(amany.getId());
+        // jiangly.block(ayman.getId());
+
+        // networkNavigation obj = new networkNavigation(jiangly.getId());
+
+        // TreeSet<Tuple> s = obj.suggestFriends(3);
+
+        // for(Tuple i : s){
+        // System.out.println(i.getFirst() + " " + i.getSecond() + " " + i.getThird());
+        // }
+        // byte error_code = FBsystem.Register("juneara@meow.asu.edu.eg", "June~Chan",
+        // "6Ara_Ara9", 'A', "9/1/1");
+        // if(error_code == 0)
+        // System.out.println("User Registered Successfully.\n");
+        // else
+        // System.out.println("Error code occured: " + error_code);
+
+        // int currUser = FBsystem.Login("juneara@meow.com", "6Ara_Ara9");
+        // if(currUser == -1)
+        // System.out.println("incorrect username or password\n");
+        // else
+        // System.out.println("Logged in successfully\n");
+
+        // Trie.insert(ayman.name,ayman.getId());
+        // Trie.insert(amany.name,amany.getId());
+        // Vector<Integer> vc=new Vector<Integer>();
+        // vc=Trie.search("A");
+        // System.out.println(vc.get(0));
+        // System.out.println(vc.get(1));
         jiangly.addFriend(ayman);
         jiangly.addFriend(khater);
         ayman.addFriend(amany);
@@ -64,34 +108,19 @@ class main {
 
         jiangly.block(amany.getId());
         jiangly.unblock(amany.getId());
-        jiangly.block(ayman.getId());
-        
-        networkNavigation obj = new networkNavigation(jiangly.getId() , 3);
-        
-        TreeSet<Tuple> s = obj.Search();
-
-        for(Tuple i : s){
-            System.out.println(i.getFirst() + " " + i.getSecond() + " " + i.getThird());
+        // jiangly.block(ayman.getId());
+        jiangly.restrictUser(khater.getId());
+        jiangly.CreatePost("Cats are really good", '+');
+        jiangly.CreatePost("cats are bad", '-');
+        Node<Post> p = FBsystem.posts.iteratorToStart();
+        while (p != null) {
+            for (Integer fe : FBsystem.users.get(ayman.getId()).feedId) {
+                if (fe == p.value().id) {
+                    System.out.println(p.value().content);
+                }
+            }
+            p = FBsystem.posts.nextValue();
         }
-        byte error_code = FBsystem.Register("juneara@meow.asu.edu.eg", "June~Chan", "6Ara_Ara9", 'A', "9/1/1"); 
-        if(error_code == 0)
-            System.out.println("User Registered Successfully.\n");
-        else
-            System.out.println("Error code occured: " + error_code);
-        
-        
-        int currUser = FBsystem.Login("juneara@meow.com", "6Ara_Ara9");
-        if(currUser == -1) 
-            System.out.println("incorrect username or password\n");
-        else
-            System.out.println("Logged in successfully\n");
-
-     Trie.insert(ayman.name,ayman.getId());
-     Trie.insert(amany.name,amany.getId());
-     Vector<Integer> vc=new Vector<Integer>();
-      vc=Trie.search("A");
-      System.out.println(vc.get(0));
-      System.out.println(vc.get(1));
 
     }
 }
--- user.java
+++ user.java
@@ -6,7 +6,6 @@ public class user {
     String name;
     private String password;
     char gender;
-    // waiting for date data type
     String Birthdate;
     Set<Integer> posts = new HashSet<Integer>();
     Set<Long> userConvs = new HashSet<Long>();
@@ -15,6 +14,7 @@ public class user {
     Set<Integer> friends = new HashSet<Integer>();
     BitSet restrictedUsers = new BitSet();
     BitSet blockedUsers = new BitSet();
+    Set<Integer> feedId = new HashSet<Integer>();
 
     user(String email, String name, String password, char gender, String Birthdate) {
         userCount++;
@@ -29,46 +29,18 @@ public class user {
 
     public void addFriend(user friend, Boolean check) {
         this.friends.add(friend.id);
-        // mp[friend].insert(this.id);
-        // map<int , user>mp;
-        // FBsystem.users.get(friend.id).addFriend(this);
     }
 
     public void addFriend(user friend) {
         this.friends.add(friend.id);
-        // mp[friend].insert(this.id);
-        // map<int , user>mp;
-        // FBsystem.users.get(friend.id).addFriend(this);
         FBsystem.users.get(friend.id).addFriend(this, false);
     }
 
     void removeFriend(int userID, Boolean check) {
-        // id of friend and his info
-        // set<pair<id,user>>s;
-        // s.erase(s.lower_bound({id,friend}));
-        /*
-         * ahmed -> mohsen
-         * ahmed - > null
-         * mohsen-> ahmed
-         * mp[mohsen.id].erase(ahmed);
-         * 
-         */
-        //
         friends.remove(userID);
     }
 
     void removeFriend(int userID) {
-        // id of friend and his info
-        // set<pair<id,user>>s;
-        // s.erase(s.lower_bound({id,friend}));
-        /*
-         * ahmed -> mohsen
-         * ahmed - > null
-         * mohsen-> ahmed
-         * mp[mohsen.id].erase(ahmed);
-         * 
-         */
-        //
         friends.remove(userID);
         FBsystem.users.get(userID).removeFriend(this.id, false);
     }
@@ -97,54 +69,78 @@ public class user {
         post.CommentId.add(test.id);
     }
 
-    void CreatePost(String content) {
-        Post test = new Post(content);
+    void CreatePost(String content, char privacy) {
+        Post test = new Post(content, privacy);
         FBsystem.posts.pushBack(test);
         this.posts.add(test.id);
+        for (Integer Cur : friends) {
+            Boolean Restricted = restrictedUsers.get(Cur);
+            if (Restricted && privacy == '-')
+                continue;
+            FBsystem.users.get(Cur).feedId.add(test.id);
+        }
+        for (Integer tagged : test.TaggedId) {
+            FBsystem.users.get(tagged).feedId.add(test.id);
+        }
     }
-    void MakeConversation(Vector<Integer> usersID){
+
+    void MakeConversation(Vector<Integer> usersID) {
         Conversation newconv = new Conversation(usersID);
         for (int i = 0; i < usersID.size(); i++) {
             FBsystem.users.get(usersID.get(i)).userConvs.add(newconv.uniqueID);
         }
         FBsystem.conversations.put(newconv.uniqueID, newconv);
     }
-    void SendMessage(String content,Long conversationId){
+
+    void SendMessage(String content, Long conversationId) {
         message msg = new message(content, this.id, conversationId);
         FBsystem.conversations.get(conversationId).messages.put(msg.messageID, msg);
     }
-    void LeaveConversation(Long convID){
+
+    void LeaveConversation(Long convID) {
         this.userConvs.remove(convID);
         FBsystem.conversations.get(convID).participantsID.remove(this.id);
-        if(FBsystem.conversations.get(convID).participantsID.size() == 0){
+        if (FBsystem.conversations.get(convID).participantsID.size() == 0) {
             FBsystem.conversations.remove(convID);
         }
     }
 
-    void restrictUser(int userID){
+    void restrictUser(int userID) {
         restrictedUsers.set(userID);
+        for (Integer myPost : posts) {
+            // add condition to check if post is restricted then
+            if (FBsystem.users.get(userID).feedId.contains(myPost)) {
+                FBsystem.users.get(userID).feedId.remove(myPost);
+            }
+        }
     }
 
-    void unrestrictUser(int userID){
+    void unrestrictUser(int userID) {
         restrictedUsers.clear(userID);
     }
 
-    void block(int userID){
-        if(FBsystem.users.get(this.id).friends.contains(userID)){
+    void block(int userID) {
+        if (FBsystem.users.get(this.id).friends.contains(userID)) {
             removeFriend(userID);
+        }
+        for (Integer myPost : posts) {
+            if (FBsystem.users.get(userID).feedId.contains(myPost)) {
+                FBsystem.users.get(userID).feedId.remove(myPost);
+            }
         }
         blockedUsers.set(userID, true);
     }
 
-    void unblock(int userID){
+    void unblock(int userID) {
         blockedUsers.set(userID, false);
     }
 
     public int getId() {
+        // getter for id
         return id;
     }
 
-    //compares a given password with the user's password
+    // compares a given password with the user's password
     boolean comparePassword(String toCompare) {
         return toCompare.equals(password);
     }
