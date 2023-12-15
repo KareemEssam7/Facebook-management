3008c5f949fd45e12a363af8b74514e57e38a055
Kareem Hatem <kareemhatem102@gmail.com>
2023-12-16 00:22:35 +0200

    login and register are now good

--- GUI/RegisterController.java
+++ GUI/RegisterController.java
@@ -6,12 +6,11 @@ import java.time.format.DateTimeFormatter;
 import System.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.text.Text;
 import javafx.scene.control.DatePicker;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -19,6 +18,9 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class RegisterController {
+
+    @FXML
+    private Text errorsText;
 
     @FXML
     private TextField emailField;
@@ -32,9 +34,6 @@ public class RegisterController {
     @FXML
     private TextField usernameField;
 
-    // @FXML
-    // private CheckBox GenderChoice;
-
     @FXML
     private ToggleGroup gender;
 
@@ -43,30 +42,31 @@ public class RegisterController {
 
     @FXML
     private void registerButtonAction(ActionEvent event) {
-        String username = usernameField.getText();
-        String email = emailField.getText();
-        String password = passwordField.getText();
-        String confirmPassword = ConfirmPasswordField.getText();
-        String genderString = gender.getSelectedToggle().toString();
-        char Gender = genderString.charAt(0);
-        String Date = Birthdate. getValue(). format(DateTimeFormatter. ofPattern("yyyy-MM-dd"));
-
-        if(!password.equals(confirmPassword))
-            System.out.println("Password doesn't match");
-        else{
-            int returnValue = FBsystem.Register(email, username, password, Gender, Date);
-            if (returnValue == 1) {
-                System.out.println("invalid email");
-            } else if (returnValue == 2) {
-                System.out.println("invalid Password");
-            } else if (returnValue == 3) {
-                System.out.println("invalid Name");
-            } else
-                System.out.println("Successful");
-            }
-            // System.out.println("Register");
-            // System.out.println("Username: " + username);
-            // System.out.println("Password: " + password);
+        try{
+            String username = usernameField.getText();
+            String email = emailField.getText();
+            String password = passwordField.getText();
+            String confirmPassword = ConfirmPasswordField.getText();
+            String genderString = gender.getSelectedToggle().toString();
+            char Gender = genderString.charAt(0);
+            String Date = Birthdate.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            if(!password.equals(confirmPassword))
+                errorsText.setText("Password doesn't match");
+            else{
+                int returnValue = FBsystem.Register(email, username, password, Gender, Date);
+                if (returnValue == 1) {
+                    errorsText.setText("Invalid email format");
+                } else if (returnValue == 2) {
+                    errorsText.setText("Password too weak");
+                } else if (returnValue == 3) {
+                    errorsText.setText("Invalid Name");
+                } else
+                    errorsText.setText("Successful");
+                }
+        }
+        catch(Exception e){
+            errorsText.setText("Please fill all fields");
+        }
     }
 
     @FXML
